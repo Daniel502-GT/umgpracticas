9b0f62c8447c97435b6147f2dc7f4a9828269446
Daniel502-GT <106029661+Daniel502-GT@users.noreply.github.com>
2022-05-28 10:03:36 -0600

    inicial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+const carrito = document.getElementById('carrito');
+const cafes = document.getElementById('lista-cafe');
+const listaCafes = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
